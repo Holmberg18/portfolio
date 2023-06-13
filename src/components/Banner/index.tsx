bbd7f1cb1924ffc0967c9962285bd73f212a9605
jon.holmberg <holmberg18@gmail.com>
2023-06-13 16:17:01 -0700

    update banner, update data

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -22,11 +22,12 @@ const Banner: FC = () => {
         <Container id="banner" fluid style={{backgroundColor: "#ededed"}} className="d-flex justify-content-center p-5">
             <Row>
                 <Col className='d-flex flex-wrap justify-content-center'>
-                    <Stack className="mx-4 py-4">
-                        <h1>{banner_data?.intro.title}</h1>
-                        <p>{banner_data?.intro.welcome}</p>
-                        <p>
+                    <Stack className="mx-4 py-5 gap-2">
+                        <h1 className="display-4">{banner_data?.intro.title}</h1>
+                        <p className="lead">{banner_data?.intro.welcome}</p>
+                        <p className="lead">
                             {banner_data.intro.tech_stack}
+                            <div className="vr h-100 align-middle border border-dark mx-1" />
                             {banner_data.stack}
                         </p>
                         <SocialLinks socialLinks={{
